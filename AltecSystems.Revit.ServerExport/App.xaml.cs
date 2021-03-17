d0b36c9ef7da3821ef9d4bb0bbf0d67c74de3c92
Alexey Kurbatov <kurbatov144@gmail.com>
2021-03-17 22:03:32 +0500

    structural change

--- AltecSystems.Revit.ServerExport/App.xaml.cs
+++ AltecSystems.Revit.ServerExport/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace RevitServerExport
+namespace AltecSystems.Revit.ServerExport
 {
     /// <summary>
     /// Interaction logic for App.xaml
